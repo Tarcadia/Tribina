7e6cdbe8c07f2205398d91c2e0d61feb5708258f
Tarcadia <tarcadia@qq.com>
2022-05-03 15:44:15 +0800

    Add stop command

---
+++ src/main/java/net/tarcadia/tribina/command/StopCommand.java
@@ -0,0 +1,13 @@
+package net.tarcadia.tribina.command;
+
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.command.ConsoleSender;
+import net.minestom.server.command.builder.Command;
+
+public final class StopCommand extends Command {
+    public StopCommand() {
+        super("stop");
+        this.setCondition(((sender, commandString) -> sender instanceof ConsoleSender));
+        this.setDefaultExecutor(((sender, context) -> MinecraftServer.stopCleanly()));
+    }
+}
