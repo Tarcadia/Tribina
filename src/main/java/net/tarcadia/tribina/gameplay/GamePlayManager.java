84522ca879b3e7625cdb93e84343f53fb71d0dba
Tarcadia <tarcadia@qq.com>
2022-05-04 02:35:15 +0800

    Add gameplay manager with some codes.

--- src/main/java/net/tarcadia/tribina/gameplay/GamePlayManager.java
+++ src/main/java/net/tarcadia/tribina/gameplay/GamePlayManager.java
@@ -1,4 +1,27 @@
 package net.tarcadia.tribina.gameplay;
 
+import org.jetbrains.annotations.NotNull;
+
 public class GamePlayManager {
+
+    private static final GamePlayManager INSTANCE = new GamePlayManager();
+
+    private GamePlayManager() {}
+
+    public static GamePlayManager getGamePlayManager() {
+        return INSTANCE;
+    }
+
+    public void init() {
+
+    }
+
+    public synchronized void register(@NotNull GamePlay gamePlay) {
+
+    }
+
+    public synchronized void unregister(@NotNull GamePlay gamePlay) {
+
+    }
+
 }
