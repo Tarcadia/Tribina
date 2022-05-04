02ad3aefc82d91a13b437cd6cda2f50d2727afd9
Tarcadia <tarcadia@qq.com>
2022-05-04 19:36:43 +0800

    add implementations that help modify the server info

--- src/main/java/net/tarcadia/tribina/ServerInfo.java
+++ src/main/java/net/tarcadia/tribina/ServerInfo.java
@@ -18,10 +18,104 @@ import java.util.UUID;
 
 public class ServerInfo {
 
-    public static String FAVICON = Favicon.fromFile(new File("./favicon.png"), Favicon.Type.PNG);
-    public static Component MOTD = Component.text("");
-    public static int MAX_PLAYER_COUNT = Integer.getInteger("tribina.max-player-count", 100);
-    public static Component MAX_PLAYER_MESSAGE = Component.text("Full Server");
+    private static String FAVICON = Favicon.fromFile(new File("./favicon.png"));
+    private static Component MOTD = Component.text("Tribina comes here.");
+    private static int MAX_PLAYER_COUNT = Integer.getInteger("tribina.max-player-count", 1);
+    private static Component MAX_PLAYER_MESSAGE = Component.translatable("multiplayer.disconnect.server_full");
+
+    @Nullable
+    public static String getFavicon() {
+        return FAVICON;
+    }
+
+    public static boolean setFaviconFromFile(@NotNull String filename) {
+        String favicon = null;
+        try {
+            File file = new File(filename);
+            favicon = Favicon.fromFile(file);
+        } catch (Throwable ignored) {}
+        if (favicon != null) {
+            FAVICON = favicon;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean setFaviconFromResource(@NotNull String filename) {
+        String favicon = null;
+        try {
+            favicon = Favicon.fromResource(filename);
+        } catch (Throwable ignored) {}
+        if (favicon != null) {
+            FAVICON = favicon;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Nullable
+    public static Component getMOTD() {
+        return MOTD;
+    }
+
+    public static boolean setMOTDPlainText(@NotNull String text) {
+        Component motd = null;
+        try {
+            motd = Component.text(text);
+        } catch (Throwable ignored) {}
+        if (motd != null) {
+            MOTD = motd;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static int getMaxPlayerCount() {
+        return MAX_PLAYER_COUNT;
+    }
+
+    public static boolean setMaxPlayerCount(int maxPlayerCount) {
+        if (maxPlayerCount >= 1) {
+            MAX_PLAYER_COUNT = maxPlayerCount;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Nullable
+    public static Component getMaxPlayerMessage() {
+        return MAX_PLAYER_MESSAGE;
+    }
+
+    public static boolean setMaxPlayerMessageTranslatable(@NotNull String translatable) {
+        Component message = null;
+        try {
+            message = Component.translatable(translatable);
+        } catch (Throwable ignored) {}
+        if (message != null) {
+            MAX_PLAYER_MESSAGE = message;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean setMaxPlayerMessageText(@NotNull String text) {
+        Component message = null;
+        try {
+            message = Component.text(text);
+        } catch (Throwable ignored) {}
+        if (message != null) {
+            MAX_PLAYER_MESSAGE = message;
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     public static void initServerInfo() {
         ConnectionManager manager = MinecraftServer.getConnectionManager();
