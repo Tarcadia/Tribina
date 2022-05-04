15e2e63cb15f6fc344fe6328f32559151033408d
Tarcadia <tarcadia@qq.com>
2022-05-05 04:42:59 +0800

    Better gameplay interface, each player should have a gameplay instance.

--- src/main/java/net/tarcadia/tribina/gameplay/GamePlay.java
+++ src/main/java/net/tarcadia/tribina/gameplay/GamePlay.java
@@ -1,8 +1,8 @@
 package net.tarcadia.tribina.gameplay;
 
-import net.minestom.server.entity.Player;
-
 public interface GamePlay {
-    void start(Player player);
-    void end(Player player);
+    void load();
+    void save();
+    void start();
+    void end();
 }
