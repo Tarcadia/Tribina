fa82151d4651c45cdf482b1bb0a7b5f531411ac4
Tarcadia <tarcadia@qq.com>
2022-05-05 21:25:09 +0800

    Implement ban and unban commands

--- src/main/java/net/tarcadia/tribina/command/ServerInfoCommand.java
+++ src/main/java/net/tarcadia/tribina/command/ServerInfoCommand.java
@@ -11,19 +11,13 @@ public class ServerInfoCommand extends Command {
         public FaviconCommand() {
             super("favicon");
             this.setCondition((sender, commandString) -> sender instanceof ConsoleSender);
-            this.setDefaultExecutor((sender, context) -> System.out.println("Syntax: \"server favicon file|res <path>\""));
+            this.setDefaultExecutor((sender, context) -> sender.sendMessage("Syntax: \"server favicon <path>\""));
             this.addSyntax((sender, context) -> {
                 String path = context.get("path");
-                boolean ret = false;
-                if (context.get("from").equals("file")) {
-                    ret = ServerInfo.setFaviconFromFile(path);
-                } else if (context.get("from").equals("res")) {
-                    ret = ServerInfo.setFaviconFromResource(path);
-                }
+                boolean ret = ServerInfo.setFavicon(path);
                 if (ret) sender.sendMessage("Server favicon set successfully.");
                 else sender.sendMessage("Server favicon set unsuccessfully.");
                 },
-                    ArgumentType.Word("from").from("file", "res"),
                     ArgumentType.String("path").setDefaultValue("favicon.png")
             );
         }
@@ -33,9 +27,9 @@ public class ServerInfoCommand extends Command {
         public MOTDCommand() {
             super("motd");
             this.setCondition((sender, commandString) -> sender instanceof ConsoleSender);
-            this.setDefaultExecutor((sender, context) -> System.out.println("Syntax: \"server motd <motd>\""));
+            this.setDefaultExecutor((sender, context) -> sender.sendMessage("Syntax: \"server motd <motd>\""));
             this.addSyntax((sender, context) -> {
-                boolean ret = ServerInfo.setMOTDPlainText(context.get("motd"));
+                boolean ret = ServerInfo.setMOTD(context.get("motd"));
                 if (ret) sender.sendMessage("Server MOTD set successfully.");
                 else sender.sendMessage("Server MOTD set unsuccessfully.");
                 },
@@ -48,7 +42,7 @@ public class ServerInfoCommand extends Command {
         public MaxPlayerCountCommand() {
             super("max-player-count");
             this.setCondition((sender, commandString) -> sender instanceof ConsoleSender);
-            this.setDefaultExecutor((sender, context) -> System.out.println("Syntax: \"server max-player-count <count>\""));
+            this.setDefaultExecutor((sender, context) -> sender.sendMessage("Syntax: \"server max-player-count <count>\""));
             this.addSyntax((sender, context) -> {
                 boolean ret = ServerInfo.setMaxPlayerCount(context.get("count"));
                 if (ret) sender.sendMessage("Server max player count set successfully.");
@@ -59,24 +53,54 @@ public class ServerInfoCommand extends Command {
         }
     }
 
-    private static class MaxPlayerMessageCommand extends Command {
-        public MaxPlayerMessageCommand() {
-            super("max-player-message");
+    private static class BanCommand extends Command {
+        public BanCommand() {
+            super("ban");
             this.setCondition((sender, commandString) -> sender instanceof ConsoleSender);
-            this.setDefaultExecutor((sender, context) -> System.out.println("Syntax: \"server max-player-message translatable|text <message>\""));
+            this.setDefaultExecutor((sender, context) -> sender.sendMessage("Syntax: \"server ban {player <username>}|{ip <user-ip>}\""));
             this.addSyntax((sender, context) -> {
-                String message = context.get("message");
-                boolean ret = false;
-                if (context.get("type").equals("translatable")) {
-                    ret = ServerInfo.setMaxPlayerMessageTranslatable(message);
-                } else if (context.get("type").equals("text")) {
-                    ret = ServerInfo.setMaxPlayerMessageText(message);
-                }
-                if (ret) sender.sendMessage("Server max player message set successfully.");
-                else sender.sendMessage("Server max player message set unsuccessfully.");
+                String username = context.get("username");
+                boolean ret = ServerInfo.banPlayer(username);
+                if (ret) sender.sendMessage("Server ban player " + username + " successfully.");
+                else sender.sendMessage("Server ban player " + username + " unsuccessfully.");
                 },
-                    ArgumentType.Word("type").from("translatable", "text"),
-                    ArgumentType.String("message")
+                    ArgumentType.Literal("player"),
+                    ArgumentType.Entity("username").onlyPlayers(true)
+            );
+            this.addSyntax((sender, context) -> {
+                String ip = context.get("user-ip");
+                boolean ret = ServerInfo.banIP(ip);
+                if (ret) sender.sendMessage("Server ban ip " + ip + " successfully.");
+                else sender.sendMessage("Server ban ip " + ip + " unsuccessfully.");
+                },
+                    ArgumentType.Literal("ip"),
+                    ArgumentType.String("user-ip")
+            );
+        }
+    }
+
+    private static class UnbanCommand extends Command {
+        public UnbanCommand() {
+            super("ban");
+            this.setCondition((sender, commandString) -> sender instanceof ConsoleSender);
+            this.setDefaultExecutor((sender, context) -> sender.sendMessage("Syntax: \"server ban {player <player>}|{ip <ip>}\""));
+            this.addSyntax((sender, context) -> {
+                String username = context.get("username");
+                boolean ret = ServerInfo.unbanPlayer(username);
+                if (ret) sender.sendMessage("Server ban player " + username + " successfully.");
+                else sender.sendMessage("Server ban player " + username + " unsuccessfully.");
+                },
+                    ArgumentType.Literal("player"),
+                    ArgumentType.Entity("username").onlyPlayers(true)
+            );
+            this.addSyntax((sender, context) -> {
+                String ip = context.get("user-ip");
+                boolean ret = ServerInfo.unbanIP(ip);
+                if (ret) sender.sendMessage("Server ban ip " + ip + " successfully.");
+                else sender.sendMessage("Server ban ip " + ip + " unsuccessfully.");
+                },
+                    ArgumentType.Literal("ip"),
+                    ArgumentType.String("user-ip")
             );
         }
     }
@@ -87,7 +111,8 @@ public class ServerInfoCommand extends Command {
         this.addSubcommand(new FaviconCommand());
         this.addSubcommand(new MOTDCommand());
         this.addSubcommand(new MaxPlayerCountCommand());
-        this.addSubcommand(new MaxPlayerMessageCommand());
-        this.setDefaultExecutor((sender, context) -> System.out.println("Syntax: \"server favicon|motd|max-player-count|max-player-message ...\""));
+        this.addSubcommand(new BanCommand());
+        this.addSubcommand(new UnbanCommand());
+        this.setDefaultExecutor((sender, context) -> sender.sendMessage("Syntax: \"server favicon|motd|max-player-count|ban|unban ...\""));
     }
 }
