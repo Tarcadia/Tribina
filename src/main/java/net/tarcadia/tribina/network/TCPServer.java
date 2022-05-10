70a28a3bca17c314883bd5b8859df0ed0cc0420f
Tarcadia <tarcadia@qq.com>
2022-05-11 00:21:43 +0800

    Move parameters to implementations

--- src/main/java/net/tarcadia/tribina/network/TCPServer.java
+++ src/main/java/net/tarcadia/tribina/network/TCPServer.java
@@ -9,39 +9,47 @@ import java.net.*;
 
 public class TCPServer extends Thread {
 
-    public static final String SYS_CONNECTION_ACCEPT_TIMEOUT = "tribina.front.connection-accept-timeout";
-    public static final String SYS_CONNECTION_RECV_TIMEOUT = "tribina.front.connection-recv-timeout";
-    public static final String SYS_CONNECTION_SEND_BUFF_SIZE = "tribina.front.connection-send-buff-size";
-    public static final String SYS_CONNECTION_RECV_BUFF_SIZE = "tribina.front.connection-recv-buff-size";
-    public static final String SYS_CONNECTION_MAX_COUNT = "tribina.front.connection-max-count";
-    public static final String SYS_PACKET_MAX_SIZE = "tribina.front.packet-max-size";
-
     public static final String SERVER_THREAD_NAME = "TCPServer.@%s";
-    public static final int CONNECTION_ACCEPT_TIMEOUT = Integer.getInteger(SYS_CONNECTION_ACCEPT_TIMEOUT, 3 * 1000);
-    public static final int CONNECTION_RECV_TIMEOUT = Integer.getInteger(SYS_CONNECTION_RECV_TIMEOUT, 1000);
-    public static final int CONNECTION_SEND_BUFF_SIZE = Integer.getInteger(SYS_CONNECTION_SEND_BUFF_SIZE, 262144);
-    public static final int CONNECTION_RECV_BUFF_SIZE = Integer.getInteger(SYS_CONNECTION_RECV_BUFF_SIZE, 32768);
-    public static final int CONNECTION_MAX_COUNT = Integer.getInteger(SYS_CONNECTION_MAX_COUNT, 128);
-    public static final int PACKET_MAX_SIZE = Integer.getInteger(SYS_PACKET_MAX_SIZE, 2097151);
 
+    private final int timeout;
     private final ServerSocket socket;
     private final Handler<Runnable> handler;
     private final Provider<Socket, Runnable> provider;
 
     private volatile boolean interrupted = false;
 
+    public TCPServer(@NotNull String host, int port, int timeout, @NotNull Handler<Runnable> handler, @NotNull Provider<Socket, Runnable> provider) throws IOException {
+        super(SERVER_THREAD_NAME.formatted(host + ":" + port));
+        this.timeout = timeout;
+        this.socket = new ServerSocket(port, 50, InetAddress.getByName(host));
+        this.socket.setSoTimeout(timeout);
+        this.handler = handler;
+        this.provider = provider;
+    }
+
+    public TCPServer(@NotNull ServerSocket socket, int timeout, @NotNull Handler<Runnable> handler, @NotNull Provider<Socket, Runnable> provider) throws IOException {
+        super(SERVER_THREAD_NAME.formatted(socket.getInetAddress().toString()));
+        this.timeout = timeout;
+        this.socket = socket;
+        this.socket.setSoTimeout(timeout);
+        this.handler = handler;
+        this.provider = provider;
+    }
+
     public TCPServer(@NotNull String host, int port, @NotNull Handler<Runnable> handler, @NotNull Provider<Socket, Runnable> provider) throws IOException {
         super(SERVER_THREAD_NAME.formatted(host + ":" + port));
+        this.timeout = 3000;
         this.socket = new ServerSocket(port, 50, InetAddress.getByName(host));
-        this.socket.setSoTimeout(CONNECTION_ACCEPT_TIMEOUT);
+        this.socket.setSoTimeout(timeout);
         this.handler = handler;
         this.provider = provider;
     }
 
     public TCPServer(@NotNull ServerSocket socket, @NotNull Handler<Runnable> handler, @NotNull Provider<Socket, Runnable> provider) throws IOException {
         super(SERVER_THREAD_NAME.formatted(socket.getInetAddress().toString()));
+        this.timeout = 3000;
         this.socket = socket;
-        this.socket.setSoTimeout(CONNECTION_ACCEPT_TIMEOUT);
+        this.socket.setSoTimeout(timeout);
         this.handler = handler;
         this.provider = provider;
     }
@@ -57,10 +65,6 @@ public class TCPServer extends Thread {
         while (!this.interrupted) {
             try {
                 Socket accepted = this.socket.accept();
-                accepted.setTcpNoDelay(true);
-                accepted.setSoTimeout(CONNECTION_RECV_TIMEOUT);
-                accepted.setSendBufferSize(CONNECTION_SEND_BUFF_SIZE);
-                accepted.setReceiveBufferSize(CONNECTION_RECV_BUFF_SIZE);
                 this.handler.handle(this.provider.provide(accepted));
             } catch (SocketTimeoutException ignored) {
             } catch (IOException e) {
