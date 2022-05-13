6af0ae48b03e93b3a12e70c8aa03da40ec6fa397
Tarcadia <tarcadia@qq.com>
2022-05-13 23:45:07 +0800

    Use the RunHandler so it is much more simple

--- src/main/java/net/tarcadia/util/function/impl/ThreadPoolHandleHandler.java
+++ src/main/java/net/tarcadia/util/function/impl/ThreadPoolHandleHandler.java
@@ -4,70 +4,27 @@ import net.tarcadia.util.function.Handler;
 import net.tarcadia.util.function.Provider;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
-
 public class ThreadPoolHandleHandler<T> implements Handler<T> {
 
-    private final String name;
-    private final BlockingQueue<Runnable> queue = new LinkedBlockingQueue<>();
-    private final Thread[] pool;
+    private final ThreadPoolRunHandler<Runnable> pool;
     private final Provider<T, Handler<T>> provider;
 
-    private boolean interrupted = false;
-
     public ThreadPoolHandleHandler(int size, @NotNull Provider<T, Handler<T>> provider) {
-        this.name = "";
-        this.pool = new Thread[size];
+        this.pool = new ThreadPoolRunHandler<>(size);
         this.provider = provider;
-        for (int i = 0; i < this.pool.length; i++) {
-            this.pool[i] = new Thread(this::run);
-            this.pool[i].start();
-        }
     }
 
     public ThreadPoolHandleHandler(@NotNull String name, int size, @NotNull Provider<T, Handler<T>> provider) {
-        this.name = name;
-        this.pool = new Thread[size];
+        this.pool = new ThreadPoolRunHandler<>(name, size);
         this.provider = provider;
-        for (int i = 0; i < this.pool.length; i++) {
-            this.pool[i] = new Thread(this::run);
-            this.pool[i].start();
-        }
-    }
-
-    public void interrupt() {
-        this.interrupted = true;
-        for (final Thread thread : this.pool) {
-            if (thread != null) thread.interrupt();
-        }
-    }
-
-    public boolean isInterrupted() {
-        return this.interrupted;
-    }
-
-    public void clear() {
-        this.queue.clear();
     }
 
     @Override
     public void handle(T t) {
         Handler<T> handler = this.provider.provide(t);
         if (handler != null) {
-            Runnable run = () -> handler.handle(t);
-            this.queue.offer(run);
+            this.pool.handle(() -> handler.handle(t));
         }
     }
 
-    private void run() {
-        while (!this.interrupted) {
-            try {
-                this.queue.take().run();
-            } catch (InterruptedException ignored) {
-            } catch (Throwable e) {
-                e.printStackTrace();
-            }
-        }
-    }
 }
